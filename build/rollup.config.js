79f11da2204ccd1c15533d53527cdcd5a51474c9
Pepe García <pgarciag93@gmail.com>
2020-04-29 12:53:12 +0200

    Refactor base config to external file

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,12 +1,10 @@
 import fs from 'fs';
-import path from 'path';
 import vue from 'rollup-plugin-vue';
-import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import minimist from 'minimist';
+import baseConfig from './rollup.config.base'
 
 // Get browserslist config and remove ie from es build targets
 const esbrowserslist = fs.readFileSync('./.browserslistrc')
@@ -16,54 +14,18 @@ const esbrowserslist = fs.readFileSync('./.browserslistrc')
 
 const argv = minimist(process.argv.slice(2));
 
-const projectRoot = path.resolve(__dirname, '..');
-
-const baseConfig = {
-  input: 'src/index.js',
-  plugins: {
-    preVue: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-      }),
-      alias({
-        resolve: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-        entries: {
-          '@': path.resolve(projectRoot, 'src'),
-        },
-      }),
-    ],
-    vue: {
-      css: true,
-      template: {
-        isProduction: true,
-      },
-    },
-    babel: {
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-    },
-  },
-};
-
-// ESM/UMD/IIFE shared settings: externals
 // Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
 const external = [
-  // list external dependencies, exactly the way it is written in the import statement.
-  // eg. 'vue'
   'vue',
 ];
 
-// UMD/IIFE shared settings: output.globals
 // Refer to https://rollupjs.org/guide/en#output-globals for details
 const globals = {
-  // Provide global variable names to replace your external imports
-  // eg. vue: 'Vue'
   vue: 'Vue',
 };
 
-// Customize configs for individual targets
 const buildFormats = [];
+
 if (!argv.format || argv.format === 'es') {
   const esConfig = {
     ...baseConfig,
@@ -148,5 +110,5 @@ if (!argv.format || argv.format === 'iife') {
   buildFormats.push(unpkgConfig);
 }
 
-// Export config
+
 export default buildFormats;
